60fc2f1f75d22eb7db30f6ffc275c49f1c75e768
marciovz <marciovz@yahoo.com.br>
2019-11-23 11:11:00 -0300

    Configurações das rotas

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,11 +47,4 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
-  settings: {
-    'import/resolver': {
-      'babel-plugin-root-import': {
-        rootPathSuffix: 'src'
-      },
-    },
-  },
 };
