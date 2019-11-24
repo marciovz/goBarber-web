6e30000d67bd137f74a545206cb6220675cf8466
marciovz <marciovz@yahoo.com.br>
2019-11-24 15:04:42 -0300

    configuração do reactotron, alterado o arquivo history de pasta

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
 
 function App() {
   return (
     <Router history={history}>
-      <Routes />;
+      <Routes />
     </Router>
   );
 }
