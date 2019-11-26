0c2d1758b92287ff858c59979b1796d18e2603ae
marciovz <marciovz@yahoo.com.br>
2019-11-26 11:16:48 -0300

    Implementado a autenticação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sesseions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuário não é um prestador');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.pushState('/dasboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
